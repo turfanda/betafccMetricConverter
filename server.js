4868a9eacea139ae5913e53e567ce6ee165875c5
Glitch (root-worm) <none>
2018-02-14 10:26:48 +0000

    👍🏋 Checkpoint README.md:596315/21 package.json:596315/33 server.js:596315/246

--- server.js
+++ server.js
@@ -3,35 +3,22 @@
 
 // init project
 var express = require('express');
+var helmet = require("helmet");
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(helmet());
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/',function(req,res){
+res.sendFile(process.cwd()+"views/index.html");
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get('/api/convert/:data',function(req,res){
+console.log(req.body.data);
 });
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
