95c878fef5e529dddd8e73250bff4f18c7409288
Glitch (betafcc-converter) <none>
2018-02-15 05:40:56 +0000

    👣🌠 Checkpoint package.json:596315/9 test.js:596315/36

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 const assert = chai.assert;
 let server = require("/server");
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
